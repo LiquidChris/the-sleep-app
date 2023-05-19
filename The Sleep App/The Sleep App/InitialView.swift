1e9bfb08ae286784d36e501f9b2b681d987e7bea
Josue Baez <josuebaez@dhcp-v121-152.mobile.uci.edu>
2023-05-19 12:40:39 -0700

    changes to main

--- The Sleep App/The Sleep App/InitialView.swift
+++ The Sleep App/The Sleep App/InitialView.swift
@@ -1,171 +1,20 @@
 //
-//  ContentView.swift
+//  InitialView.swift
 //  The Sleep App
 //
-//  Created by Iyad Hassan on 5/17/23.
+//  Created by Josue Baez on 5/19/23.
 //
-
-// import SwiftUI
-// 
-// struct ContentView: View {
-//     var body: some View {
-//         VStack {
-//             Image(systemName: "globe")
-//                 .imageScale(.large)
-//                 .foregroundColor(.accentColor)
-//             Text("Hello, world!")
-//         }
-//         .padding()
-//     }
-// }
-
-// struct ContentView_Previews: PreviewProvider {
-//     static var previews: some View {
-//         ContentView()
-//     }
-// }
-
 
 import SwiftUI
 
-struct Question: Identifiable {
-    let id = UUID()
-    let text: String
-}
-
-struct ContentView: View {
-    @State private var questions: [Question] = [
-        Question(text: "Question 1"),
-        Question(text: "Question 2"),
-        Question(text: "Question 3")
-    ]
-    
-    @State private var answers: [UUID: String] = [:]
-    @State private var isQuestionnaireComplete = false
-    
+struct InitialView: View {
     var body: some View {
-        if isQuestionnaireComplete {
-            AppSidebarNavigation()
-        } else {
-            QuestionPromptView(questions: $questions, answers: $answers, isComplete: $isQuestionnaireComplete)
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct QuestionPromptView: View {
-    @Binding var questions: [Question]
-    @Binding var answers: [UUID: String]
-    @Binding var isComplete: Bool
-    
-    var body: some View {
-        NavigationView {
-            VStack {
-                List(questions) { question in
-                    NavigationLink(destination: AnswerView(question: question, answer: $answers[question.id])) {
-                        Text(question.text)
-                    }
-                }
-                
-                Button(action: {
-                    isComplete = true
-                }) {
-                    Text("Submit")
-                }
-            }
-            .navigationTitle("Questionnaire")
-        }
-    }
-}
-
-struct AnswerView: View {
-    let question: Question
-    @Binding var answer: String?
-    
-    var body: some View {
-        VStack {
-            Text(question.text)
-            TextField("Answer", text: $answer ?? "")
-        }
-        .padding()
-    }
-}
-
-struct Screen1View: View {
-    var body: some View {
-        Text("Screen 1")
-            .font(.title)
-            .padding()
-    }
-}
-
-struct Screen2View: View {
-    var body: some View {
-        Text("Screen 2")
-            .font(.title)
-            .padding()
-    }
-}
-
-struct Screen3View: View {
-    var body: some View {
-        Text("Screen 3")
-            .font(.title)
-            .padding()
-    }
-}
-
-struct AppSidebarNavigation: View {
-    enum NavigationItem {
-        case screen1, screen2, screen3
-    }
-    
-    @State private var selectedItem: NavigationItem?
-    
-    var body: some View {
-        NavigationView {
-            if selectedItem == nil {
-                VStack {
-                    NavigationLink(destination: Screen1View(), tag: .screen1, selection: $selectedItem) {
-                        Button("Screen 1") {
-                            selectedItem = .screen1
-                        }
-                    }
-                    NavigationLink(destination: Screen2View(), tag: .screen2, selection: $selectedItem) {
-                        Button("Screen 2") {
-                            selectedItem = .screen2
-                        }
-                    }
-                    NavigationLink(destination: Screen3View(), tag: .screen3, selection: $selectedItem) {
-                        Button("Screen 3") {
-                            selectedItem = .screen3
-                        }
-                    }
-                }
-                .padding()
-            } else {
-                List(selection: $selectedItem) {
-                    NavigationLink(destination: Screen1View(), tag: .screen1, selection: $selectedItem) {
-                        Label("Screen 1", systemImage: "1.circle")
-                    }
-                    NavigationLink(destination: Screen2View(), tag: .screen2, selection: $selectedItem) {
-                        Label("Screen 2", systemImage: "2.circle")
-                    }
-                    NavigationLink(destination: Screen3View(), tag: .screen3, selection: $selectedItem) {
-                        Label("Screen 3", systemImage: "3.circle")
-                    }
-                }
-                .listStyle(SidebarListStyle())
-                .navigationTitle("App Sidebar")
-            }
-        }
-    }
-}
-
-@main
-struct QuestionnaireApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+struct InitialView_Previews: PreviewProvider {
+    static var previews: some View {
+        InitialView()
     }
 }
